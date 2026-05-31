8d01c52c678c8b8c5e59d9a8ee129cf96c1211f3
LutChihiro <1363741477@qq.com>
2026-05-31 16:45:37 +0800

    项目基础搭建（后端）

---
+++ src/main/java/com/lifeviews/LifeViewsApplication.java
@@ -0,0 +1,12 @@
+package com.lifeviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeViewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeViewsApplication.class, args);
+    }
+}
---
+++ src/main/java/com/lifeviews/common/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.lifeviews.common;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Result<Void> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+        return Result.fail(HttpStatus.BAD_REQUEST.value(), message);
+    }
+
+    @ExceptionHandler(BindException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Result<Void> handleBindException(BindException ex) {
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+        return Result.fail(HttpStatus.BAD_REQUEST.value(), message);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Result<Void> handleConstraintViolationException(ConstraintViolationException ex) {
+        return Result.fail(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Result<Void> handleException(Exception ex) {
+        return Result.fail(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
+    }
+}
